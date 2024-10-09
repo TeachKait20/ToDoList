f0cd133c6cb11924d6fa92134ee1a20853638b5b
TeachKait20 <pgodunov@kait20.ru>
2024-10-09 15:26:44 +0300

    Обновление программы to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -1,1 +1,28 @@
-# ready
+# Путь к файлу
+file_path = "README.md"
+
+def add_task(task):
+    with open(file_path, "a") as file:
+        file.write(f"- [ ] {task}\n")
+    print(f"Задача '{task}' добавлена в README.md")
+
+def main():
+    while True:
+        command = input("Введите команду (add/show/complete/delete/exit): ")
+
+        if command == 'add':
+            task = input("Введите задачу: ")
+            add_task(task)
+        elif command == 'show':
+            pass  # Вывод всех заметок
+        elif command == 'complete':
+            pass  # Попросить ввести номер задачи для отметки выполнения. Проверить есть ли такая задача.
+        elif command == 'delete':
+            pass  # Удалить задачу
+        elif command == 'exit':
+            print("Выход...")
+            break
+        else:
+            print("Неизвестная команда. Попробуйте ещё раз.")
+
+main()
